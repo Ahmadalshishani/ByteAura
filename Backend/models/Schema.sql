b77bc2680178b4ae7716146f43b18f878af17eeb
Ahmadalshishani <ahmad.alshishanii@gmail.com>
2024-10-13 00:32:30 +0300

    data_base

---
+++ Backend/models/Schema.sql
@@ -0,0 +1,63 @@
+DROP TABLE role;
+
+CREATE TABLE roles
+(
+    id SERIAL NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+DROP TABLE users;
+
+CREATE TABLE users
+(
+    id SERIAL NOT NULL,
+    username VARCHAR (50) UNIQUE,
+    password VARCHAR (50) NOT NULL,
+    role_id INT,
+    is_deleted SMALLINT DEFAULT 0,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    PRIMARY KEY (id)
+
+);
+
+DROP TABLE categorys;
+
+CREATE TABLE categorys
+(
+    id SERIAL NOT NULL,
+    title VARCHAR (50) UNIQUE,
+    is_deleted SMALLINT DEFAULT 0,
+    PRIMARY KEY (id)
+
+);
+
+DROP TABLE products ;
+
+CREATE TABLE products
+(
+    id SERIAL NOT NULL,
+    image VARCHAR (5000),
+    title VARCHAR (255),
+    description text,
+    price INT,
+    category_id INT,
+    is_deleted SMALLINT DEFAULT 0 ,
+    FOREIGN KEY (category_id)REFERENCES categorys(id),
+    PRIMARY KEY (id)
+
+);
+
+DROP TABLE orders ;
+ 
+ CREATE TABLE orders 
+ (
+    order_id SERIAL NOT NULL,
+    user_id INT,
+    product_id INT,
+    is_deleted SMALLINT DEFAULT 0,
+    FOREIGN KEY (user_id)REFERENCES users(id),
+    FOREIGN KEY (product_id)REFERENCES products(id),
+    PRIMARY KEY (order_id)
+
+ )
---
+++ Backend/models/db.js
@@ -0,0 +1,13 @@
+const {Pool}= require("pg")
+const connectionString = process.env.DB_URL;
+
+const pool = new Pool({
+    connectionString
+})
+pool .connect()
+.then((res)=>{
+    console.log(`DB conected to ${res.database}` );
+}).catch((err)=>{
+    console.log(err);
+})
+module.exports = {pool}
