754470500236bec212e5510ffae46476f2133b2a
Ahmadalshishani <ahmad.alshishanii@gmail.com>
2024-10-13 00:48:11 +0300

    authentication

---
+++ Backend/middlewares/authentication.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const authentication = (req, res, next) => {
+  try {
+    if (!req.headers.authentication)
+      res.status(403).json({ message: "Forbidden" });
+    const token = req.headers.authorization.split(" ").pop();
+    jwt.verify(token, process.env.SECRET, (err, result) => {
+      if (err) {
+        res.status(403).json({
+          success: false,
+          message: "The token is invalid or expired",
+        });
+      } else {
+        req.token = result;
+        next();
+      }
+    });
+  } catch (error) {
+    res.status(403).json({ message: "Forbidden" });
+  }
+};
+module.exports = authentication;
