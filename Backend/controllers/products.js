33f606555761334906568807302e3ba7da5f2233
Ahmadalshishani <ahmad.alshishanii@gmail.com>
2024-10-13 22:55:11 +0300

    get all products

---
+++ Backend/controllers/products.js
@@ -0,0 +1,47 @@
+const { pool } = require("../models/db");
+
+//! insert a product to the web by admin
+
+const insertProduct = (req, res) => {
+  const { image, title, description, price, category_id } = req.body;
+  pool
+    .query(
+      "INSERT INTO products (image,title,description,price,category_id) VALUES ($1,$2,$3,$4,$5) RETURNING * ;",
+      [image, title, description, price, category_id]
+    )
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+        message: "Product have been inserted",
+        result: result.rows,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server error",
+        err: err,
+      });
+    });
+};
+
+// get all products for main page
+
+const getAllProducts = (req, res) => {
+    pool
+      .query(`SELECT * FROM products WHERE is_deleted=0;`)
+      .then((result) => {
+        res.status(200).json({
+          success: true,
+          message: "All products",
+          result: result.rows,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          success: false,
+          message: "Server error",
+          err: err,
+        });
+      });
+  };
